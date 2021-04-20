f1bd276500e7468288d6dce179f76b95b423064d
rafael eduardo <rafaelMejia@rafaels-MacBook-Pro-2.local>
2021-04-19 23:08:40 -0600

    notifications1

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -91,9 +91,7 @@ export default {
           })
           this.event = this.createFreshEventObject()
         })
-        .catch(() => {
-          console.log('There was a problem creating your event.')
-        })
+        .catch(()=>{})
     }
   }
 }
